0796a9ffa170912d60e29c4da891898696e3cdaf
Prince-Joy-Ahmed-TT <134224832+Masud-Hridoye404@users.noreply.github.com>
2024-08-17 14:07:57 +0600

    Update joy2.js

--- scripts/commands/joy2.js
+++ scripts/commands/joy2.js
@@ -26,7 +26,7 @@ const media = (
 
 	if (body.indexOf("😒")==0 || body.indexOf("😒")==0) {
 		var msg = {
-				body: "𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "𝐑𝐀𝐉 𝐀𝐇𝐌𝐄𝐃",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
