a92ee8d990c7a751feeb63666d7442831861ca71
Prince-Joy-Ahmed-TT <134224832+Masud-Hridoye404@users.noreply.github.com>
2024-08-17 14:10:24 +0600

    Update npx10.js

--- scripts/commands/npx10.js
+++ scripts/commands/npx10.js
@@ -26,7 +26,7 @@ const media = (
 
 	if (body.indexOf("maya")==0 || body.indexOf("maiya")==0 || body.indexOf("maya")==0 || body.indexOf("মায়া")==0 || body.indexOf("Maya")==0 || body.indexOf("Maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0) {
 		var msg = {
-				body: "𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "𝐑𝐀𝐉 𝐀𝐇𝐌𝐄𝐃",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
